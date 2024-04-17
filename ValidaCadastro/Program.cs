77151953f4a9cefe97693e25dc50d4b70afcbb17
Alan Gabriel Giacomoni Przyvara <88240260+YiipHuhu@users.noreply.github.com>
2024-04-17 12:53:28 -0300

    Add files via upload

---
+++ ValidaCadastro/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ValidacaoLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usuario = "Alan";
+            string senha = "admin";
+
+            Console.WriteLine("Digite o usuario: ");
+            string usuarioDigitado = Console.ReadLine();
+            Console.WriteLine("Digite a senha: ");
+            string senhaDigitada = Console.ReadLine();
+
+            if (usuario == usuarioDigitado && senha == senhaDigitada)
+            {
+                Console.WriteLine($"{usuarioDigitado}, Login sucess!");
+            }
+            else
+            {
+                Console.WriteLine($"{usuarioDigitado}, Failed login.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+           //int numero = int.Parse(Console.ReadLine());
---
+++ teste vogal/program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace TesteVogalConsoante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma letra: ");
+            string letra = Console.ReadLine();
+
+            if (letra == "1" || letra == "2" || letra == "3" || letra == "4" || letra == "5" || letra == "6" || letra == "7" || letra == "8" || letra == "9" || letra == "0")
+            {
+                Console.WriteLine("O valor digitado é um número");
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+            }
+
+            else if (letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u")
+            {
+                Console.WriteLine("A letra digitada é uma vogal");
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+            }
+            else
+            {
+                Console.WriteLine("A letra digitada é uma consoante");
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+            }
+
+            Console.ReadKey();
+                Console.Clear();
+                Main(args);
+        }
+    }
+}
